bdc30ed2566b6e556a0e3a66dc745bb2cd8b1198
aliosha7891 <aliosha7891@gmail.com>
2018-10-15 14:58:33 +0300

    Change nodemon to npm for starting a server

--- server/server.js
+++ server/server.js
@@ -2,10 +2,11 @@ const path = require('path');
 const express = require('express');
 
 const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
-    console.log('Server started at 3000');
+app.listen(port, () => {
+    console.log(`Server started at ${port}`);
 });
